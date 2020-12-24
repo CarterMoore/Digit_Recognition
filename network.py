40f9fedc646fef100bd220f200ffe6c6eb886a6c
= <=>
2020-12-23 20:29:08 -0500

    Added backpropagation

--- network.py
+++ network.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 class Network:
 
@@ -17,10 +18,12 @@ class Network:
         '''Takes activations from input layer and uses weights and biases
         to compute activation for each subsequent layer'''
         for b, w in zip(self.biases, self.weights):
-            activation = sigmoid(w@activation+b)
+            activation = sigmoid(np.dot(w,activation)+b)
         return activation
 
     def stochastic_gradient_descent(self, training_data, epochs, mini_batch_size, eta, test_data=None):
+        '''Train network with stochastic gradient descent, training_data is list of tuples (x,y) with 
+        training inputs and required outputs.'''
         if test_data:
             n_test = len(test_data)
         n = len(training_data)
@@ -35,14 +38,47 @@ class Network:
                 print("Epoch {} complete".format(j))
 
     def update_mini_batch(self, mini_batch, eta):
-        nabla_b = [np.zeros(b.shape) for b in self.biases)]
-        nabla_w = [np.zeros(w.shape) for w in self.weights)]
+        '''Perform gradient descent on one mini batch. mini_batch is list of tuples, eta is learning rate'''
+        nabla_b = [np.zeros(b.shape) for b in self.biases] # Gradient for biases
+        nabla_w = [np.zeros(w.shape) for w in self.weights] # Gradient for weights
+        # Compute gradient for each x,y pair and add to total gradient
         for x, y in mini_batch:
             delta_nabla_b, delta_nabla_w = self.backprop(x,y)
             nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
-            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nable_w)]
+            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        # Average gradient and multiply by learning rate, then use to update weights and biases
         self.weights = [w-(eta/len(mini_batch))*nw for w, nw in zip(self.weights, nabla_w)]
         self.biases = [b-(eta/len(mini_batch))*nb for b, nb in zip(self.biases, nabla_b)]
+        
+    def backprop(self, x, y):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        activation = x
+        activations = [x]
+        z_vectors = []
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w,activation)+b
+            z_vectors.append(z)
+            activation = sigmoid(z)
+            activations.append(activation)
+        delta = self.cost_derivative(activations[-1], y) * sigmoid(z_vectors[-1], True)
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta,np.transpose(activations[-2]))
+        for l in range(2, self.num_layers):
+            z = z_vectors[-l]
+            sp = sigmoid(z, True)
+            delta = np.dot(np.transpose(self.weights[-l+1]),delta) * sp
+            nabla_b[-l] = delta
+            nabla_w[-l] = np.dot(delta,np.transpose(activations[-l-1]))
+        return(nabla_b, nabla_w)
+
+    def cost_derivative(self, output_activations, y):
+        return (output_activations-y)
+
+    def evaluate(self,test_data):
+        test_results = [(np.argmax(self.feedforward(x)), y)
+                        for (x, y) in test_data]
+        return sum(int(x == y) for (x, y) in test_results)
 
 def sigmoid(z, derivative=False):
     '''Applies sigmoid function elementwise'''
@@ -51,8 +87,6 @@ def sigmoid(z, derivative=False):
     return 1/(1 + np.exp(-z))
 
 if __name__ == '__main__':
-    n = Network([2, 3, 5])
-    input_a = [0.8, 0.42]
-    output = n.feedforward(input_a)
-    print(output)
-
+    net = Network([1,2,1])
+    inputs = [[0,0,0,0],[0,0,0,1],[0,0,1,0],[0,1,0,0]]
+    print(np.array(inputs[0]))
